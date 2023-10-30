c5446f4fab1f7e2b42bb21342e3588c431c20984
Jun <105594827+JunDeve@users.noreply.github.com>
2023-10-30 16:09:38 +0900

    20231030_backend_v.1.1

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,72 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const googleTrends = require('google-trends-api');
+const axios = require("axios");
+const googleTrends = require("google-trends-api");
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('백엔드 시작화면');
+app.get("/", (req, res) => {
+  res.send("백엔드 시작화면");
 });
 
 // 트렌드 중 실시간 인기(24시간 올카테고리)
-app.get('/trending', (req, res) => {
-  const apiKey = 'AIzaSyDlCtE421Jns3qDxRM5U6kLrRwvxNIXL7U';
+app.get("/trending", (req, res) => {
+  const apiKey = "AIzaSyDlCtE421Jns3qDxRM5U6kLrRwvxNIXL7U";
   googleTrends.apiKey = apiKey;
 
-  googleTrends.realTimeTrends({
-    geo: 'US' , category: 'all'
-  }, function(err, results) {
+  googleTrends.realTimeTrends(
+    {
+      geo: "US",
+      category: "all",
+    },
+    function (err, results) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("실시간 트렌드 : ", results);
+        // res.json(results);
+        res.json(JSON.parse(results));
+      }
+    }
+  );
+});
+
+// 트렌드 중 일일별 인기
+app.get("/daytrending", (req, res) => {
+  const apiKey = "AIzaSyDlCtE421Jns3qDxRM5U6kLrRwvxNIXL7U";
+  googleTrends.apiKey = apiKey;
+  
+  googleTrends.dailyTrends({
+    trendDate: new Date('2023-10-30'),
+    geo: 'KR',
+  }, function (err, results) {
     if (err) {
       console.log(err);
     } else {
-      console.log('실시간 트렌드 : ', results);
-      // res.json(results);
+      console.log("일일 트렌드 : ", results);
       res.json(JSON.parse(results));
     }
-  })
+  });
 });
+
+app.get("/search", async (req, res) => {
+  const KEY = '400cee3fddff018623f67a238776b71999f8345693a1353b190ced2c7700deb2';
+  const keyword = req.query.q;
+
+  try {
+    const response = await axios.get(
+      `https://serpapi.com/search.json?engine=google&q=Coffee`
+      // `https://serpapi.com/search.json?engine=google&q=${keyword}&location=South Korea&hl=ko&gl=kr&google_domain=google.co.kr&num=10&start=10&safe=active&api_key=${KEY}`
+    );
+    const searchData = response.data;
+    res.json(searchData);
+  } catch (error) {
+    console.error("요청 중 오류 발생:", error);
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`>>>> 서버 실행중 : http://localhost:${port}/ <<<<`);
--- src/App.js
+++ src/App.js
@@ -1,33 +1,76 @@
-import './App.css';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
+import "./App.css";
+import axios from "axios";
+import React, { useEffect, useState } from 'react';
 
 function App() {
   const [trends, setTrends] = useState([]);
+  const [searchResults, setSearchResults] = useState([]);
+  const [daytrends, setDayTrends]=  useState([]);
+  const keyword = 'abc';
 
+  // 구글 서치 데이터
   useEffect(() => {
-
-    axios.get('http://localhost:5000/trending')
+    axios.get(`http://localhost:5000/search?q=${keyword}`)
       .then(response => {
-        const data = response.data;
-        console.log('백엔드에서 받은 데이터:', data);
-
-        const topTrendingStories = data.storySummaries.trendingStories.slice(0, 5);
-        setTrends(topTrendingStories);
+        const searchData = response.data;
+        console.log("백엔드에서 받은 검색 결과:", searchData);
+        const firstTenResults = searchData.organic_results.slice(0, 5);
+        setSearchResults(firstTenResults);
       })
       .catch(error => {
-        console.error('요청 중 오류 발생:', error);
+        console.error("요청 중 오류 발생:", error);
+      });
+  }, [keyword]);
+
+  // 구글 트랜드 데이터
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/trending")
+      .then(response => {
+        const trendData = response.data;
+        console.log("백엔드에서 받은 트렌드 데이터:", trendData);
+
+        const topTrendingStories = trendData.storySummaries.trendingStories.slice(0, 5);
+        setTrends(topTrendingStories);
+      })
+      .catch((error) => {
+        console.error("요청 중 오류 발생:", error);
       });
 
+      axios
+      .get("http://localhost:5000/daytrending")
+      .then(response => {
+        const daytrendData = response.data;
+        console.log("백엔드에서 받은 일일 트렌드 데이터:", daytrendData);
+
+        // 여기를 고쳐야합니다. 일일 트렌드 데이터 경로는 default 안에 있는 trendingSearchesDays 배열 안에 있는 trendingSearches 배열안에 있는 title 과 formattedTraffic 을 가져오면 됩니다.
+        // const DayTrendingStories  = daytrendData.default.trendingSearchesDays.trendingSearches.slice(0,5);
+        // setDayTrends(DayTrendingStories);
+      })
+      .catch((error) => {
+        console.error("요청 중 오류 발생:", error);
+      });
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>codehive : 실시간 인기 검색어</p>
+        <p>서치확인</p>
+        <ul>
+          {searchResults.map((result, index) => (
+            <ul key={index}>{result.title} <hr/> {result.link}</ul>
+          ))}
+        </ul>
+        <p>실시간 인기 검색어</p>
         <ul>
           {trends.map((story, index) => (
             <li key={index}>{story.title}</li>
+          ))}
+        </ul>
+        <p>일별 인기 급상승 검색어</p>
+        <ul>
+          {daytrends.map((daystory, index) => (
+            <li key={index}>{daystory.title}</li>
           ))}
         </ul>
       </header>
