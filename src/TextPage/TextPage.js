958f29bc99ce8bb49a8a6a22df6a3fed226cd547
kimjonguk1 <148532342+kimjonguk1@users.noreply.github.com>
2023-11-07 09:56:19 +0900

    20202361_Frontv1
    프론트 작업 - 뒤로가기 + 메인 버튼 구현

--- src/TextPage/TextPage.js
+++ src/TextPage/TextPage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
-
+import NavigationButtons from './NewButton';
 const App = () => {
     const [canvasPosition, setCanvasPosition] = useState({ left: 0, top: 0 });
     const [clickedButtonName, setClickedButtonName] = useState(null);
@@ -12,6 +12,10 @@ const App = () => {
         y: (index + 1) * 100,
         level: 0
     }));
+
+    const [history, setHistory] = useState([]);
+  const [showNavigation, setShowNavigation] = useState(false);
+  const [navigationPosition, setNavigationPosition] = useState({ x: 0, y: 0 });
     useEffect(() => {
         console.log("이동이 완료되었습니다.");
     }, [canvasPosition]);
@@ -23,13 +27,9 @@ const App = () => {
     const canvasRef = useRef(null);
     useEffect(() => {
         const canvas = canvasRef.current;
-        const ctx = canvas.getContext('2d');
         const resizeCanvas = () => {
             canvas.width = window.innerWidth;
             canvas.height = window.innerHeight;
-            ctx.strokeStyle = 'black';
-            ctx.lineWidth = 10;
-            ctx.strokeRect(0, 0, canvas.width, canvas.height);
         };
         resizeCanvas();
         window.addEventListener('resize', resizeCanvas);
@@ -37,22 +37,65 @@ const App = () => {
             window.removeEventListener('resize', resizeCanvas);
         };
     }, []);
-    const handleButtonClick = (name, x, y, level) => {
-        const newButtons = list2.map((item, index) => {
-            const position = calcButtonPosition(x, y, index, level);
-            return { name: item, x: position.x, y: position.y, level: level + 1 };
-        });
+    const handleButtonClick = (name, x, y) => {
+        // Save current state to history before making changes
+        setHistory(prevHistory => [
+            ...prevHistory, 
+            { 
+                buttons: buttons.map(button => ({ ...button })),
+                canvasPosition, 
+                clickedButtonName, 
+                showNavigation,
+                navigationPosition  // 이 부분을 추가합니다.
+            }
+        ]);
+        
+        document.body.style.overflow = 'hidden';
         const clickedButton = buttons.find(button => button.name === name);
+        
         setClickedButtonName(name);
+        
         const centerX = x + cSize / 2;
         const centerY = y + cSize / 2;
+      
+        // 클릭된 버튼의 중심 좌표를 로그합니다.
         console.log(`클릭된 버튼의 중심 좌표: (${centerX}, ${centerY})`);
-        setButtons([clickedButton, ...newButtons]); // 새로운버튼
+      
+        // 클릭된 버튼만 남기고 모든 다른 버튼을 제거합니다.
+        // setButtons([clickedButton]); // 이전 코드를 주석처리하거나 삭제합니다.
+        
+        // Add only the clicked button and the random buttons.
+        const initialNewButtons = list2.map((item, index) => ({
+          name: item,
+          x: centerX - cSize / 2,
+          y: centerY - cSize / 2,
+          transitioning: true,
+        }));
+      
+        // 새로운 버튼들을 상태에 추가합니다.
+        setButtons([clickedButton, ...initialNewButtons]);
+      
+        // 캔버스의 위치를 조정합니다.
         setCanvasPosition({
-            left: (window.innerWidth / 2 - centerX) * canvasScale,
-            top: (window.innerHeight / 2 - centerY) * canvasScale
-        }); 
-    };
+          left: (window.innerWidth / 2 - centerX) * canvasScale,
+          top: (window.innerHeight / 2 - centerY) * canvasScale
+        });
+
+        // 네비게이션 위치 설정
+        setNavigationPosition({ x, y });
+
+      // 3초 후에 새로운 버튼 목록으로 업데이트
+      setTimeout(() => {
+        const newButtons = list2.map((item, index) => {
+          const position = calcButtonPosition(x, y, index);
+          return { name: item, x: position.x, y: position.y };
+        });
+        setButtons([clickedButton, ...newButtons]);
+      
+        
+        setShowNavigation(true);
+      }, 1000); 
+  };
     const calcButtonPosition = (x, y, index) => {
         const size = cSize;
         switch (index) {
@@ -65,51 +108,82 @@ const App = () => {
             default: return { x, y };
         }
     };
-    return (
 
-        
+    //HOME버튼
+    const handleHomeClick = () => {
+    setButtons(list1); // 초기 버튼 리스트로 설정
+    setCanvasPosition({ left: 0, top: 0 }); // 캔버스 위치 초기화
+    setShowNavigation(false); // 네비게이션 숨김
+    setClickedButtonName(null); // 클릭된 버튼 상태를 초기화
+
+    };
+
+    const handleBackClick = () => {
+        console.log('뒤로 가기 버튼 클릭됨', history);
+        if (history.length > 0) {
+            const lastHistoryState = history[history.length - 1];
+    
+            // 히스토리에서 상태 복원
+            setButtons(lastHistoryState.buttons);
+            setCanvasPosition(lastHistoryState.canvasPosition);
+            setClickedButtonName(lastHistoryState.clickedButtonName);
+            setShowNavigation(lastHistoryState.showNavigation);
+            setNavigationPosition(lastHistoryState.navigationPosition); // 이 부분을 추가합니다.
+    
+            // 히스토리 업데이트 (비동기적으로 할 필요가 없으므로 setTimeout을 제거했습니다)
+            setHistory(prevHistory => prevHistory.slice(0, prevHistory.length - 1));
+        } else {
+            console.log("더 이상 뒤로 갈 수 없습니다.");
+            setShowNavigation(true); // 더 이상 뒤로 갈 수 없다면 네비게이션을 표시합니다.
+        }
+    };
+      
+    
+    
+    return (
         <div style={{
             position: 'relative',
             transform: `translate(${canvasPosition.left}px, ${canvasPosition.top}px) scale(${canvasScale})`,
             transition: 'transform 0.5s ease-in-out'
         }}>
             <canvas ref={canvasRef} style={{ position: 'absolute', zIndex: -1 }} />
-            {buttons.map((item, index) => (
-                <button
-                    key={index}
-                    onClick={() => handleButtonClick(item.name, item.x, item.y, item.level)}
-                    style={{
-                        position: 'absolute',
-                        left: item.name === clickedButtonName ? `${item.x-cSize*3.5}px`:`${item.x}px`,
-                        top: item.name === clickedButtonName ? `${item.y-cSize*3.5}px`:`${item.y}px`,
-                        borderRadius: '50%',
-                        display: 'flex',
-                        alignItems: 'center',
-                        justifyContent: 'center',
-                        border: 'none',
-                        cursor: 'pointer',
-                        backgroundColor: item.name === clickedButtonName ? 'skyblue' : 'gray',
-                        transition: 'background-color 0.5s ease-in-out, transform 0.5s ease-in-out ',
-                        width: item.name === clickedButtonName ? `${cSize*8}px`:`${cSize}px`,
-                        height: item.name === clickedButtonName ? `${cSize*8}px`:`${cSize}px`,
-                    }}
-                >
-                    {item.name}
-                </button>
-            ))}
-
-
-            
-        </div>
-        
+            {
+    buttons.map((item, index) => (
+        <button
+            key={index}
+            onClick={() => handleButtonClick(item.name, item.x, item.y)}
+            style={{
+                position: 'absolute',
+                borderRadius: item.name === clickedButtonName ? '25%' :'50%',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                border: 'none',
+                cursor: 'pointer',
+                backgroundColor: item.name === clickedButtonName ? 'skyblue' : 'gray',
+                
+                transition: 'background-color 0.5s ease-in-out, width 1s ease-in-out, height 1s ease-in-out, left 1s ease-in-out, top 1s ease-in-out',
+                width: item.name === clickedButtonName ? `${cSize * 13}px` : `${cSize}px`,
+                height: item.name === clickedButtonName ? `${cSize * 8}px` : `${cSize}px`,
+                
+                left: item.name === clickedButtonName ? `${item.x - cSize * 6}px` : `${item.x}px`,
+                top: item.name === clickedButtonName ? `${item.y - cSize * 3.5}px` : `${item.y}px`,
+            }}
+        >
+            {item.name}
+        </button>
+    ))
+}
+ {showNavigation && (
+        <NavigationButtons
+          buttonName={clickedButtonName}
+          position={navigationPosition}
+          onHomeClick={handleHomeClick}
+          onBackClick={handleBackClick}
+        />
+      )}
+    </div>
         
     );
 };
 export default App;
-
-
-
-
-
-
-
